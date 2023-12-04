919f15bfe9f071ac99d293f727f9ea5ba0b231cc
Rishant Sharma <s222458666@deakin.edu.au>
2023-12-04 06:46:23 +0530

    Add sidebar

---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Layout, Compass } from "lucide-react";
+
+const guestRoutes = [
+    {
+        icon: Layout,
+        label: "Dashboard",
+        href: "/",
+    },
+    {
+        icon: Compass,
+
+    }
+]
+
+export const SidebarRoutes = () => {
+    return (
+        <div className="flex flex-col w-full">
+
+        </div>
+    )
+}
