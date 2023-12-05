129651af81a56898db7356e43498b0746778427b
Rishant Sharma <s222458666@deakin.edu.au>
2023-12-05 06:50:21 +0530

    Complete Sidebar

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Layout, Compass } from "lucide-react";
+import { SidebarItem } from "./sidebar-item";
 
 const guestRoutes = [
     {
@@ -10,14 +11,23 @@ const guestRoutes = [
     },
     {
         icon: Compass,
-
-    }
+        label: "Browse",
+        href: "/search",
+    },
 ]
 
 export const SidebarRoutes = () => {
+    const routes = guestRoutes;
     return (
         <div className="flex flex-col w-full">
-
+            {routes.map((route) => (
+                <SidebarItem
+                key={route.href}
+                icon={route.icon}
+                label={route.label}
+                href={route.href}
+                />
+            ))}
         </div>
     )
 }
